d6871d92a4228057da57020d2d766fccebaf4554
Co-Ji <100311029+Co-Ji@users.noreply.github.com>
2022-04-15 21:45:50 +0900

    메인에서 버튼 엘레먼츠 가져옴

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -1,79 +1,88 @@
-import { size } from "lodash";
 import React from "react";
 import styled from "styled-components";
 
 const Button = (props) => {
-  const { purple, text, _onClick, children, margin, width, padding, fontSize, bold } = props;
+    const {
+        purple,
+        text,
+        _onClick,
+        children,
+        margin,
+        width,
+        padding,
+        fontSize,
+        bold,
+    } = props;
 
-  const styles = {
-    margin: margin,
-    width: width,
-    padding: padding,
-    fontSize: fontSize,
-    bold: bold,
-  };
+    const styles = {
+        margin: margin,
+        width: width,
+        padding: padding,
+        fontSize: fontSize,
+        bold: bold,
+    };
 
-  if (purple) {
+    if (purple) {
+        return (
+            <React.Fragment>
+                <PurpleBtn {...styles} onClick={_onClick}>
+                    {text ? text : children}
+                </PurpleBtn>
+            </React.Fragment>
+        );
+    }
+
     return (
-      <React.Fragment>
-        <PurpleBtn {...styles} onClick={_onClick}>
-          {text ? text : children}
-        </PurpleBtn>
-      </React.Fragment>
+        <React.Fragment>
+            <WhiteBtn {...styles} onClick={_onClick}>
+                {text ? text : children}
+            </WhiteBtn>
+        </React.Fragment>
     );
-  }
-
-  return (
-    <React.Fragment>
-      <WhiteBtn {...styles} onClick={_onClick}>
-        {text ? text : children}
-      </WhiteBtn>
-    </React.Fragment>
-  );
 };
 
 Button.defaultProps = {
-  purple: false,
-  text: false,
-  children: null,
-  _onClick: () => {},
-  margin: false,
-  width: false,
-  padding: false,
-  fontSize: false,
-  bold: false,
+    purple: false,
+    text: false,
+    children: null,
+    _onClick: () => {},
+    margin: false,
+    width: false,
+    padding: false,
+    fontSize: false,
+    bold: false,
 };
 
 const PurpleBtn = styled.button`
-  width: ${(props) => props.width};
-  border: 1px solid #5f0080;
-  border-radius: 3px;
-  max-width: 100%;
-  height: 54px;
-  background-color: #5f0080;
-  color: #ffffff;
-  font-size: ${(props) => props.fontSize};
-  font-weight: ${(props) => (props.bold ? "700" : "500")};
-  text-align: center;
-  box-sizing: border-box;
-  ${(props) => (props.margin? `margin: ${props.margin};` : "")}
-  ${(props) => (props.padding? `padding: ${props.padding};` : "")}
+    width: ${(props) => props.width};
+    border: 1px solid #5f0080;
+    border-radius: 3px;
+    max-width: 100%;
+    height: 54px;
+    background-color: #5f0080;
+    color: #ffffff;
+    font-size: ${(props) => props.fontSize};
+    font-weight: ${(props) => (props.bold ? "700" : "500")};
+    text-align: center;
+    box-sizing: border-box;
+    ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+    ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
 `;
 
 const WhiteBtn = styled.button`
-  width: ${(props) => props.width};
-  border: 1px solid #5f0080;
-  border-radius: 3px;
-  max-width: 100%;
-  height: 54px;
-  background-color: #ffffff;
-  color: #5f0080;
-  font-size: 16px;
-  font-weight: bold;
-  text-align: center;
-  box-sizing: border-box;
-  ${(props) => (props.margin? `margin: ${props.margin};` : "")}
-  ${(props) => (props.padding? `padding: ${props.padding};` : "")}
+    width: ${(props) => props.width};
+    border: 1px solid #5f0080;
+    border-radius: 3px;
+    max-width: 100%;
+    height: 54px;
+    background-color: #ffffff;
+    color: #5f0080;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+    box-sizing: border-box;
+    ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+    ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
 `;
 
 export default Button;
