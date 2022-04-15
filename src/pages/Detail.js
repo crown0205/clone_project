b47e60daf4b1847cfba2ec9e57debba938d480d0
Co-Ji <100311029+Co-Ji@users.noreply.github.com>
2022-04-15 21:39:26 +0900

    디테일페이지 작성중

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -4,7 +4,17 @@ import styled from "styled-components";
 const Detail = () => {
     return (
         <React.Fragment>
-            <h1>Detail</h1>
+            <div>
+                <div>
+                    <div>이미지</div>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+                <div>배너이미지</div>
+                <div>상품설명, 고객리뷰, 문의내용</div>
+                <div>상세설명이미지</div>
+            </div>
         </React.Fragment>
     );
 };
