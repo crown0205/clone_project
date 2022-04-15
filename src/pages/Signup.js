54db6246e93274158a41e9673d9b6caacb6ac9e6
jiaegong <jiae.gong93@gmail.com>
2022-04-15 21:45:23 +0900

    commit

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,17 +1,11 @@
 import React from "react";
 
 const Signup = () => {
-<<<<<<< HEAD
   return (
     <React.Fragment>
       <h1>Signup</h1>
     </React.Fragment>
   )
 }
-=======
-
-
-};
->>>>>>> jiae
 
 export default Signup;
