8f4d2c3eca1f883f4db53b2f8e6d6cdaae7f54a9
jiaegong <jiae.gong93@gmail.com>
2022-04-15 21:46:26 +0900

    commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,11 @@
 import React from "react";
 
 const Login = () => {
-<<<<<<< HEAD
   return (
     <React.Fragment>
       <h1>Login</h1>
     </React.Fragment>
   )
 }
-=======
-
-
-};
->>>>>>> jiae
 
 export default Login;
