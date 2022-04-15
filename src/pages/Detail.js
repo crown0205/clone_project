ea13247ad619569df44bf1e5539d8190f4435edf
Co-Ji <100311029+Co-Ji@users.noreply.github.com>
2022-04-15 23:24:28 +0900

    디테일페이지 기본틀

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 const Detail = () => {
-  return (
-    <React.Fragment>
-      <h1>Detail</h1>
-    </React.Fragment>
-  )
-}
+    return (
+        <React.Fragment>
+            <div>
+                <div>
+                    <div>이미지</div>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+                <div>배너이미지</div>
+                <div>상품설명, 고객리뷰, 문의내용</div>
+                <div>상세설명이미지</div>
+            </div>
+        </React.Fragment>
+    );
+};
 
 export default Detail;
