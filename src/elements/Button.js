65102c9cd910244d10d9610a33e32d60a66ec942
jiaegong <jiae.gong93@gmail.com>
2022-04-16 14:24:10 +0900

    Update: Signup page view

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -2,15 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = (props) => {
-  const { purple, text, _onClick, children, margin, width, padding, fontSize, bold } = props;
+  const { purple, text, _onClick, children, margin, width, height, padding, fontSize, bold, flex } = props;
 
-  const styles = {
-    margin: margin,
-    width: width,
-    padding: padding,
-    fontSize: fontSize,
-    bold: bold,
-  };
+  const styles = { margin, width, height, padding, fontSize, bold, flex };
 
   if (purple) {
     return (
@@ -38,25 +32,32 @@ Button.defaultProps = {
   _onClick: () => {},
   margin: false,
   width: "",
+  height: "44px",
   padding: false,
-  fontSize: "",
+  fontSize: "16px",
   bold: false,
+  flex: false,
 };
 
 const PurpleBtn = styled.button`
   width: ${(props) => props.width};
+  height: ${(props) => props.height};
   border: 1px solid #5f0080;
   border-radius: 3px;
   max-width: 100%;
-  height: 54px;
   background-color: #5f0080;
   color: #ffffff;
+  font-family: "Noto Sans KR";
   font-size: ${(props) => props.fontSize};
   font-weight: ${(props) => (props.bold ? "700" : "500")};
   text-align: center;
   box-sizing: border-box;
   ${(props) => (props.margin? `margin: ${props.margin};` : "")}
   ${(props) => (props.padding? `padding: ${props.padding};` : "")}
+  ${(props) => (props.flex? `display: flex;` : "")}
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const WhiteBtn = styled.button`
@@ -64,15 +65,20 @@ const WhiteBtn = styled.button`
   border: 1px solid #5f0080;
   border-radius: 3px;
   max-width: 100%;
-  height: 54px;
+  height: 44px;
   background-color: #ffffff;
   color: #5f0080;
-  font-size: 16px;
+  font-family: "Noto Sans KR";
+  font-size: ${(props) => props.fontSize};
   font-weight: bold;
   text-align: center;
   box-sizing: border-box;
   ${(props) => (props.margin? `margin: ${props.margin};` : "")}
   ${(props) => (props.padding? `padding: ${props.padding};` : "")}
+  ${(props) => (props.flex? `display: flex; justify-content: center; align-items: center;` : "")}
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export default Button;
--- src/elements/Input.js
+++ src/elements/Input.js
@@ -2,9 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const Input = (props) => {
-  const { type, placeholder, value, _onChange } = props;
+  const { type, placeholder, value, _onChange, width, height, margin } = props;
 
-  return <Textarea type={type} placeholer={placeholder} value={value} onChange={_onChange}/>;
+  const styles = { width, height, margin };
+
+  return (
+    <Textarea
+      {...styles}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={_onChange}
+    />
+  );
 };
 
 Input.defaultProps = {
@@ -13,14 +23,21 @@ Input.defaultProps = {
   value: "",
   _onChange: () => {},
   width: "332px",
+  height: "44px",
+  margin: false,
 };
 
 const Textarea = styled.input`
   border: 1px solid #333333;
   width: ${(props) => props.width};
   max-width: 100%;
-  height: "44px";
-  padding: "0 14px 0 14px";
+  height: ${(props) => props.height};
+  margin: ${(props) => props.margin};
+  padding: 0 14px 0 14px;
   box-sizing: border-box;
+  border-radius: 3px;
+  .placeholder {
+    color: #cccccc;
+  }
 `;
 export default Input;
